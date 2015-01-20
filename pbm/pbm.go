d17bf6258d6d355af1093fdd73fc24d93d8c9b2e
Scott Pakin <scott+gpg@pakin.org>
2015-01-19 23:47:51 -0700

    Code cleanup: introduce netpbmReader structure
    Moved functions from DecodeConfig to methods of a new netpbmReader
    structure.  The intention is for netpbmReader to be used to parse all
    Netpbm formats, not just raw PBM.

--- pbm/pbm.go
+++ pbm/pbm.go
@@ -10,13 +10,76 @@ import (
 	"unicode"
 )
 
+// A netpbmReader extends bufio.Reader with the ability to read bytes
+// and numbers while skipping over comments.
+type netpbmReader struct {
+	*bufio.Reader        // Inherit Read, UnreadByte, etc.
+	err          error  // Sticky error state
+	oneByte      []byte // Buffer into which to read a single byte
+}
+
+// newNetpbmReader allocates, initializes, and returns a new netpbmReader.
+func newNetpbmReader(r *bufio.Reader) *netpbmReader {
+	return &netpbmReader{Reader: r, oneByte: make([]byte, 1)}
+}
+
+// Err returns the netpbmReader's current error state.
+func (nr netpbmReader) Err() error {
+	return nr.err
+}
+
+// GetNextByteAsRune returns the next byte, cast to a rune, or 0 on error (and
+// errors are sticky).
+func (nr *netpbmReader) GetNextByteAsRune() rune {
+	if nr.err != nil {
+		return 0
+	}
+	_, nr.err = nr.Read(nr.oneByte)
+	if nr.err != nil {
+		return 0
+	}
+	return rune(nr.oneByte[0])
+}
+
+// GetNextInt returns the next base-10 integer read from a netpbmReader,
+// skipping whitespace and comments.
+func (nr *netpbmReader) GetNextInt() int {
+	// Find the first digit.
+	var c rune
+	for nr.err == nil && !unicode.IsDigit(c) {
+		for c = nr.GetNextByteAsRune(); unicode.IsSpace(c); c = nr.GetNextByteAsRune() {
+		}
+		if c == '#' {
+			// Comment -- discard the rest of the line.
+			for c = nr.GetNextByteAsRune(); c != '\n'; c = nr.GetNextByteAsRune() {
+			}
+		}
+	}
+	if nr.err != nil {
+		return -1
+	}
+
+	// Read while we have base-10 digits.  Return the resulting int.
+	value := int(c - '0')
+	for c = nr.GetNextByteAsRune(); unicode.IsDigit(c); c = nr.GetNextByteAsRune() {
+		value = value*10 + int(c-'0')
+	}
+	if nr.err != nil {
+		return -1
+	}
+	nr.err = nr.UnreadByte()
+	if nr.err != nil {
+		return -1
+	}
+	return value
+}
+
 // DecodeConfig reads and parses the PBM header.
 func DecodeConfig(r io.Reader) (image.Config, error) {
 	// The PBM header is ASCII.  Define a bunch of helper functions to
 	// parse it.
-	rb := bufio.NewReader(r)
+	nr := newNetpbmReader(bufio.NewReader(r))
 	var err error
-	b := make([]byte, 1)
 	badness := func() (image.Config, error) {
 		// Something went wrong.  Either we have an error code to
 		// explain what or we make up a generic error message.
@@ -25,60 +88,17 @@ func DecodeConfig(r io.Reader) (image.Config, error) {
 		}
 		return image.Config{}, err
 	}
-	getNextByte := func() byte {
-		// Return the next byte or 0 on error (and errors are sticky).
-		if err != nil {
-			return 0
-		}
-		_, err = rb.Read(b)
-		if err != nil {
-			return 0
-		}
-		return b[0]
-	}
-	getNextInt := func() int {
-		value := -1
-
-		// Find the first digit.
-		var b byte
-		for err == nil && !unicode.IsDigit(rune(b)) {
-			for b = getNextByte(); unicode.IsSpace(rune(b)); b = getNextByte() {
-			}
-			if b == '#' {
-				// Comment -- discard the rest of the line.
-				for b = getNextByte(); b != '\n'; b = getNextByte() {
-				}
-			}
-		}
-		if err != nil {
-			return -1
-		}
-
-		// Read while we have base-10 digits.  Return the resulting int.
-		value = int(b - '0')
-		for b = getNextByte(); unicode.IsDigit(rune(b)); b = getNextByte() {
-			value = value*10 + int(b-'0')
-		}
-		if err != nil {
-			return -1
-		}
-		err = rb.UnreadByte()
-		if err != nil {
-			return -1
-		}
-		return value
-	}
 
 	// A PBM file header is "P4", followed by whitespace, followed by a
 	// width in pixels, followed by whitespace, followed by a height in
 	// pixels, followed by a single whitespace.
-	if getNextByte() != 'P' || getNextByte() != '4' || !unicode.IsSpace(rune(getNextByte())) {
+	if nr.GetNextByteAsRune() != 'P' || nr.GetNextByteAsRune() != '4' || !unicode.IsSpace(nr.GetNextByteAsRune()) {
 		return badness()
 	}
 	var cfg image.Config
-	cfg.Width = getNextInt()
-	cfg.Height = getNextInt()
-	if err != nil || !unicode.IsSpace(rune(getNextByte())) {
+	cfg.Width = nr.GetNextInt()
+	cfg.Height = nr.GetNextInt()
+	if nr.Err() != nil || !unicode.IsSpace(nr.GetNextByteAsRune()) {
 		return badness()
 	}
 
